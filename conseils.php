d890f881ba1f92db69ee92f0c932eb79c2f98608
SPrinceK <133128180+SPrinceK@users.noreply.github.com>
2024-05-22 12:42:33 +0200

    Update conseils.php

--- conseils.php
+++ conseils.php
@@ -66,7 +66,7 @@ $articles = get_articles();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/conseil.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Votre plateforme de conseils</title>
    
 </head>
