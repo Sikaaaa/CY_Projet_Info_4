2adf38959460df068407ca2ec01ad6d78cba22b1
SPrinceK <133128180+SPrinceK@users.noreply.github.com>
2024-05-22 12:40:56 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -19,7 +19,7 @@ session_destroy();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/deconnexion.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Votre plateforme de conseils</title>
 
 </head>
