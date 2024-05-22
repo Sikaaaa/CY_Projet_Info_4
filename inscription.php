3fb419d8c4a1e8178a6f790700a9c00cd618bddb
SPrinceK <133128180+SPrinceK@users.noreply.github.com>
2024-05-22 12:40:26 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -31,7 +31,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/inscription.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Votre plateforme de conseils</title>
 
 </head>
